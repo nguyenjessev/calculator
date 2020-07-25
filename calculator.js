5794ff5a06f4b443700879ebe4ab4e832af6b355
Jesse <nguyenjessev@gmail.com>
2020-07-25 11:46:57 -0400

    Add number button click handler skeleton

---
+++ calculator.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operation, num1, num2) {
+  switch(operation) {
+    case 'add':
+      return add(num1, num2);
+    case 'subtract':
+      return subtract(num1, num2);
+    case 'multiply':
+      return multiply(num1, num2);
+    case 'divide':
+      return divide(num1, num2);
+    default:
+      alert('Invalid operation');
+  }
+}
+
+const numButtons = document.querySelectorAll('.number');
+numButtons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    console.log(`You clicked ${btn.dataset.value}`);
+  })
+})
